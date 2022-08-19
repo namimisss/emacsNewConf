892f9c4eff1421afb7a3d001cd583ec970cf2be7
sakurabeaver <877435317@qq.com>
2022-08-19 22:07:14 +0800

    eglot config

--- config/jconfig.el
+++ config/jconfig.el
@@ -16,6 +16,7 @@
 ;; eglot
 ;;(add-to-list 'eglot-server-programs '(cc-mode . ("ccls")))
 ;;(add-to-list 'eglot-server-programs `(cc-mode . ("ccls")))
+(add-to-list 'eglot-server-programs '(c++-mode . ("ccls")))
 (add-hook 'c++-mode-hook 'eglot-ensure)
 (add-hook 'c-mode-hook 'eglot-ensure)
 ;;(add-hook 'c++mode-hook 'cc-mode)
