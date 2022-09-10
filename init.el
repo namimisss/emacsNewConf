356237f1b6e9abec601d2da80ac3b62e93bec248
namimisss <877435317@qq.com>
2022-09-10 10:20:25 +0800

    add cmake exe path

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cmake-mode-cmake-executable "/usr/bin/cmake")
  '(package-selected-packages
    '(helm-rtags ivy-rtags rtags company-irony-c-headers company-irony irony-eldoc irony modern-cpp-font-lock disaster demangle cmake-mode use-package)))
 (custom-set-faces
